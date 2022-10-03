ea4c6d8b5c392d49a0c60c906b1389bc46261f35
Qingyu Jiang <jiangqyvictor@gmail.com>
2022-10-03 16:33:22 -0700

    Victor

--- src/main/java/cpen221/mp1/ngrams/NGrams.java
+++ src/main/java/cpen221/mp1/ngrams/NGrams.java
@@ -1,9 +1,33 @@
 package cpen221.mp1.ngrams;
 
-import java.util.List;
-import java.util.Map;
+import java.text.BreakIterator;
+import java.util.*;
+
 
 public class NGrams {
+
+    private List<Map<String, Long>> wordss;
+
+    String[] wordsarray;
+
+    private String[] getWords(String text) {
+        ArrayList<String> words = new ArrayList<>();
+        BreakIterator wb = BreakIterator.getWordInstance();
+        wb.setText(text);
+        int start = wb.first();
+        for (int end = wb.next();
+             end != BreakIterator.DONE;
+             start = end, end = wb.next()) {
+            String word = text.substring(start, end).toLowerCase();
+            word = word.replaceAll("^\\s*\\p{Punct}+\\s*", "").replaceAll("\\s*\\p{Punct}+\\s*$", "");
+            if (!word.equals(" ")) {
+                words.add(word);
+            }
+        }
+        String[] wordsArray = new String[words.size()];
+        words.toArray(wordsArray);
+        return wordsArray;
+    }
 
     /**
      * Create an NGrams object
@@ -12,7 +36,10 @@ public class NGrams {
      *             is not null and is not empty.
      */
     public NGrams(String[] text) {
-
+        wordsarray=new String[text.length];
+        for (int i=0;i<text.length;i++){
+            wordsarray[i]=text[i];
+        }
     }
 
     /**
@@ -26,7 +53,22 @@ public class NGrams {
      * 2-grams, ..., n-grams
      */
     public long getTotalNGramCount(int n) {
-        return -1; // TODO: Implement this method
+        long totalcount=0;
+        for(int x=0;x<wordsarray.length;x++){
+            String[] words=getWords(wordsarray[x]);
+            for(int i=1;i<=n;i++){
+                Set<String> seti=new HashSet<>();
+                for(int j=0;j<words.length-i;j++){
+                    String iwords="";
+                    for(int k=j;k<i+j;k++) {
+                        iwords += words[k];
+                    }
+                    seti.add(iwords);
+                }
+                totalcount += seti.size();
+            }
+        }
+        return totalcount; // TODO: Implement this method
     }
 
     /**
@@ -36,6 +78,40 @@ public class NGrams {
      * with the i-th entry being all the (i+1)-grams and their counts
      */
     public List<Map<String, Long>> getAllNGrams() {
-        return null; // TODO: Implement this method
+        long count=0;
+        int max=0;
+        for(int x=0;x<wordsarray.length;x++) {
+            String[] words = getWords(wordsarray[x]);
+            if(words.length>=max){
+                max= words.length;
+            }
+        }
+        List<Map<String, Long>> maplist = new ArrayList<>(max);
+        for(int x=0;x<wordsarray.length;x++) {
+            String[] words = getWords(wordsarray[x]);
+            for (int i = 1; i <= words.length; i++) {
+                Map<String, Long> map = new HashMap<>();
+                List<String> igram = new ArrayList<>();
+                for (int j = 0; j <= words.length - i; j++) {
+                    String iwords = "";
+                    for (int k = j; k < i + j; k++) {
+                        iwords += words[k];
+                    }
+                    String iwordsString = String.join(" ", iwords);
+                    igram.add(iwordsString);//creates a list that contains all i-grams
+                }
+                for (String s : igram) {
+                    for (int l = 0; l < igram.size(); l++) {
+                        count = 0;
+                        if (s.equals(igram.get(i))) {
+                            count++;
+                        }
+                    }
+                    map.put(s, count);
+                }
+                maplist.add(map);
+            }
+        }
+        return maplist; // TODO: Implement this method
     }
 }
