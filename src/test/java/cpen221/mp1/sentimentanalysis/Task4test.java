4b6304aec27660f461d5b1bdf18f961daf33d94b
Qingyu Jiang <jiangqyvictor@gmail.com>
2022-10-14 18:37:52 -0700

    Victor 21851241

--- src/test/java/cpen221/mp1/sentimentanalysis/Task4test.java
+++ src/test/java/cpen221/mp1/sentimentanalysis/Task4test.java
@@ -4,12 +4,23 @@ import org.junit.jupiter.api.Test;
 import java.io.FileNotFoundException;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class Task4 {
+public class Task4test {
     @Test
     public void testRating1() {
         try {
             SentimentAnalyzer rmp_sa = new SentimentAnalyzer("data/review1.txt");
-            assertEquals(4f, rmp_sa.getPredictedRating("good teacher"));
+            assertEquals(5f, rmp_sa.getPredictedRating("good teacher"));
+        }
+        catch (FileNotFoundException fnf) {
+            fail("Data file is not in the right place!");
+        }
+    }
+
+    @Test
+    public void testRating2() {
+        try {
+            SentimentAnalyzer rmp_sa = new SentimentAnalyzer("data/review2.txt");
+            assertEquals(5f, rmp_sa.getPredictedRating("good"));
         }
         catch (FileNotFoundException fnf) {
             fail("Data file is not in the right place!");
